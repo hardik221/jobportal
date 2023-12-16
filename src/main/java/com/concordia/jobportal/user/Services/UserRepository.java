2a6410888515263df14b9e2419d22e815dfc373f
Hardik Amareliya <hardikamareliya221@gmail.com>
2023-12-16 16:17:00 -0500

    register and login endpoint added for user

--- src/main/java/com/concordia/jobportal/user/Services/UserRepository.java
+++ src/main/java/com/concordia/jobportal/user/Services/UserRepository.java
@@ -5,11 +5,12 @@ import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.data.mongodb.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, Integer> {
     List<User> findAll();
 
-    @Query("{'name' : ?0, 'password' : ?1}")
-    User findByNameAndPassword(String name, String password);
+    @Query("{'email' : ?0, 'password' : ?1}")
+    Optional<User> findByEmailAndPassword(String email, String password);
 
 }
